6111c3877023a5b5084054787e1159386e7cc9ad
Alex-Yordan <x.yordan@gmail.com>
2025-01-27 22:27:00 +0300

    cod
    исправления

--- Dz.py
+++ Dz.py
@@ -1,13 +1,17 @@
 class User():
     def __init__(self, ID, name, access):
 
-        self.name = ID
-        self.voice = name
-        self.color = access
+        self._ID = ID
+        self._user_name = name
+        self._access = access
 
 class Admin(User):
     def __init__(self, add_user, rem_user):
-        super().__init__("")
+        super().__init__()
 
-        self.add = add_user
-        self.remove = rem_user
+
+
+
+user1 = User("40020", "Uriy", "5")
+user2 = User("40030", "Ulia", "2")
+admin1 = Admin("100", "", "5")
